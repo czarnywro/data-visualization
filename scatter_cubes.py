f9ed18eea4afbd3db112253dd75ab4c99015eba7
79106717+czarnywro@users.noreply.github.com <79106717+czarnywro@users.noreply.github.com>
2022-04-28 15:10:49 +0200

    1 chapter of tutorial done

---
+++ scatter_cubes.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
+
+plt.style.use("seaborn")
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
+
+#Zdefiniowanie tytułu wykresu i etykiek osi
+ax.set_title("Sześciany liczb", fontsize=24)
+ax.set_xlabel("Wartość", fontsize=14)
+ax.set_ylabel("Sześciany wartości", fontsize=14)
+
+#Zdefiniowanie zakresu dla kadej osi
+ax.axis([0, 5100, 0, 100000020000])
+
+plt.show()
