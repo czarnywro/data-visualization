31cbbf414f80896350dbc4bd337ab582eb2bc0a1
79106717+czarnywro@users.noreply.github.com <79106717+czarnywro@users.noreply.github.com>
2022-07-25 11:49:42 +0200

    Kolejne lekcje związane z plikami CSV, JSON i API

---
+++ API_excercise/python_repos.py
@@ -0,0 +1,23 @@
+import requests
+
+#Wykonanie wywołania API i zachowanie otrzymanej odpowiedzi.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Kod stanu: {r.status_code}")
+#Umieszczenie odpowiedzi API w zmiennej
+response_dict = r.json()
+print(f"Calkowita liczba repozytoriów: {response_dict['total_count']}")
+
+#Przetworzenie informacji o repozytoriach
+repo_dicts = response_dict['items']
+print(f"Liczba zwróconych repozytoriów: {len(repo_dicts)}")
+
+print('\nWybrane informacje o kazdym repozytorium:')
+
+for repo_dict in repo_dicts:
+    print(f"\nNazwa: {repo_dict['name']}")
+    print(f"Właściciel: {repo_dict['owner']['login']}")
+    print(f"Gwiazdki: {repo_dict['stargazers_count']}")
+    print(f"Repozytorium: {repo_dict['html_url']}")
+    print(f"Opis: {repo_dict['description']}")
---
+++ CSV_excercise/death_valley_lows.py
@@ -0,0 +1,35 @@
+import csv
+
+filename = 'CSV_excercise/data/death_valley_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+#     dates, highs, lows = [], [], []
+#     for row in reader:
+#         current_date = datetime.strptime(row[2], "%Y-%m-%d")
+#         high = int(row[5])
+#         low = int(row[6])
+#         dates.append(current_date)
+#         highs.append(high)
+#         lows.append(low)
+
+# #Dane wykresu
+# plt.style.use("seaborn")
+# fig, ax = plt.subplots()
+# ax.plot(dates, highs, c="red", alpha=0.5)
+# ax.plot(dates, lows, c="blue", alpha=0.5)
+# ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+
+# #Formatowanie wykresu
+# ax.set_title("Największa i najnizsza temp dnia - 2018", fontsize=24)
+# ax.set_xlabel("", fontsize=16)
+# fig.autofmt_xdate()
+# ax.set_ylabel("Temperatura (F)", fontsize=16)
+# ax.tick_params(axis="both", which="major", labelsize=16)
+
+# plt.show()
