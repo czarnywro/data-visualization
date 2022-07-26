97c5f8c1bb0359fcd758c21d7db25ac0c4d35800
79106717+czarnywro@users.noreply.github.com <79106717+czarnywro@users.noreply.github.com>
2022-07-26 19:27:14 +0200

    Nowe ćwiczenia z API

---
+++ API_excercise/hn_submissions.py
@@ -0,0 +1,33 @@
+from operator import itemgetter
+
+import requests
+
+#Wykonunanie wywołania API i zachowanie otrzymanej odpowiedzi
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print(f"Kod stanu: {r.status_code}")
+
+#Przetworzenie informacji o kadym artykule
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:10]:
+    #Przygotowanie oddzielnego wywołania API dla kazdego artykułu
+    url = f'https://hacker-news.firebaseio.com/v0/item/{submission_id}.json'
+    r = requests.get(url)
+    print(f"id: {submission_id}\tstatus: {r.status_code}")
+    response_dict = r.json()
+
+    #Utworzenie słownika dla kazdego artykułu
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+        'comments': response_dict['descendants'],
+    }
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\nTytuł artykułu: {submission_dict['title']}")
+    print(f"Łącze do dyskusji: {submission_dict['hn_link']}")
+    print(f"Liczba komentarzy: {submission_dict['comments']}")
